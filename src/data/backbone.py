879be0ae1b0351a98b1be9d87ac1f29873e756f1
Sanghyun Son <thstkdgus35@snu.ac.kr>
2019-07-15 00:43:49 +0900

    move preprocessing code to another function

--- src/data/backbone.py
+++ src/data/backbone.py
@@ -71,11 +71,16 @@ class RestorationData(data.Dataset):
             # Finish the implementation with pickle
             pass
 
-        x, y = pp.crop(x, y, p=self.p, training=self.training)
-        x, y = pp.set_channel(x, y, c=self.c)
+        x, y = self.preprocess(x, y)
+
+        return x, y
+
+    def preprocess(self, x, y):
+        #x, y = pp.crop(x, y, p=self.p, training=self.training)
+        #x, y = pp.set_channel(x, y, c=self.c)
         # If you've implemented the augmentation
         #x, y = pp.augment(x, y)
-        x, y = pp.to_tensor(x, y)
+        #x, y = pp.to_tensor(x, y)
 
         return x, y
 
