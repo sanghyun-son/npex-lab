0ba42abb0ccf54d7f3536be04ce67d861dba7640
Sanghyun Son <thstkdgus35@snu.ac.kr>
2019-07-18 15:12:17 +0900

    generate lr images

---
+++ src/generate_lr.py
@@ -0,0 +1,20 @@
+import os
+import glob
+from PIL import Image
+
+scales = [2, 4]
+for split in ('train', 'eval'):
+    img_list = sorted(glob.glob('../DIV2K_sub/{}/target/*.png'.format(split)))
+    for img_name in img_list:
+        # img: np.uint8
+        hr = Image.open(img_name)
+        w, h = hr.size
+        for s in scales:
+            os.makedirs('../DIV2K_sub/{}/input_x{}'.format(split, s), exist_ok=True)
+            lw = w // s
+            lh = h // s
+
+            lr = hr.resize((lw, lh), resample=Image.BICUBIC)
+            lr.save(img_name.replace('target', 'input_x{}'.format(s)))
+
+
